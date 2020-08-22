9972644408e307103d10722b8339847f1e43705a
Justin Miller <justinmiller171@gmail.com>
2020-08-22 03:09:58 -0700

    added slug generator and more validation

--- back-end/api/v1/url.js
+++ back-end/api/v1/url.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = require("express").Router();
+const { nanoid } = require('nanoid');
 
 function isValidURL(string) {
   let res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
   return (res !== null)
 };
+
+const isAlphaNumeric = ch => {
+	return ch.match(/^[a-z0-9]+$/i) !== null;
+}
 
 router.get("/", (req, res, next) => {
   res.send("<h1>URL Home</h1>");
@@ -14,17 +19,26 @@ router.get("/create/", (req, res, next) => {
   // todo: create url
   if (!isValidURL(req.query.originalUrl)) {
     res.json({
-      message: "No valid URL provided",
+      message: "URL is missing or is not a valid url",
     });
   }
-  
+
   if (req.query.customSlug === undefined) {
     // todo: generate random slug
-    slug = 1234;
+    console.log("Generating custom slug")
+    slug = nanoid(7);
+    // todo: add to db
   } else {
-    slug = req.query.customSlug;
+    if(isAlphaNumeric(req.query.customSlug) && req.query.customSlug.length < 20) {
+      console.log(req.query.customSlug.length);
+      slug = req.query.customSlug;
+      // todo: add to db
+    } else {
+      res.json({
+        message: "Invalid slug, must contain only alpha-numeric characters and be under 20 characters"
+      })
+    }
   }
-
   res.json({
     originalUrl: req.query.originalUrl,
     customSlug: slug,
